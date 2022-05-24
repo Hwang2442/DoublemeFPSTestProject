6b1aa475f7ea0e7b32966c4754e97d6d90e7b803
Hwang2442 <68269611+Hwang2442@users.noreply.github.com>
2022-05-25 00:27:28 +0900

    시간 기록 클래스 추가

---
+++ Assets/Scripts/TimeRecorder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimeRecorder : MonoBehaviour
+{
+    public float RecordingTime { get; private set; }
+
+    Coroutine m_coroutine;
+
+    public void StartRecording(UnityAction<float> onUpdate)
+    {
+        if (m_coroutine != null)
+        {
+            Debug.LogWarning("Already recording...");
+
+            return;
+        }
+
+        RecordingTime = 0;
+        m_coroutine = StartCoroutine(Co_TimeRecording(onUpdate));
+    }
+
+    public void StopRecording()
+    {
+        if (m_coroutine == null)
+        {
+            Debug.LogWarning("Not recording...");
+
+            return;
+        }
+
+        StopCoroutine(m_coroutine);
+    }
+
+    private IEnumerator Co_TimeRecording(UnityAction<float> onUpdate)
+    {
+        while (true)
+        {
+            yield return null;
+
+            RecordingTime += Time.deltaTime;
+            onUpdate?.Invoke(RecordingTime);
+        }
+    }
+}
